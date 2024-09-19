59851464ffe1254d5f1ebd15bcbe396baaa40a28
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2024-09-20 02:27:19 +0530

    Allow existing user to sign in via google

--- helpers/passport.js
+++ helpers/passport.js
@@ -30,31 +30,32 @@ exports.setUpPassport = (passport) => {
   );
 
   passport.use(
-    new GoogleStrategy({
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:4000/auth/google/callback"
-    },
-    async (accessToken, refreshToken, profile, done)=>{
-      try {
-        const existingUser = await User.findOne({ googleId: profile.id });
-        if (existingUser) {
-          return done(null, existingUser);
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: 'http://localhost:4000/auth/google/callback',
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          const existingUser = await User.findOne({
+            $or: [{ email: profile.emails[0].value }, { googleId: profile.id }],
+          });
+          if (existingUser) {
+            return done(null, existingUser);
+          }
+          const newUser = new User({
+            googleId: profile.id,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            email: profile.emails[0].value,
+          });
+          await newUser.save();
+          done(null, newUser);
+        } catch (err) {
+          done(err, null);
         }
-        const newUser = new User({
-          googleId: profile.id,
-          firstName: profile.name.givenName,
-          lastName: profile.name.familyName,
-          email: profile.emails[0].value,
-        });
-        console.log(newUser)
-        await newUser.save();
-        done(null, newUser);
-      } catch (err) {
-        console.log(err);
-        done(err, null);
       }
-  }
-  )
+    )
   );
 };
