a97f66a305722fb5e194ab4e8304db0db083d004
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2024-09-20 17:07:42 +0530

    Add hosted url to env

--- helpers/passport.js
+++ helpers/passport.js
@@ -34,7 +34,7 @@ exports.setUpPassport = (passport) => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback',
+        callbackURL: `${process.env.HOSTED_URL}/auth/google/callback`,
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
